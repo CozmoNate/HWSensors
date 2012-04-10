38f45912d68bbee98dce972e6abc925ca6ebf9d3
kozlek <mozodojo@gmail.com>
2012-04-10 16:54:30 +0300

    Added GTX560 id, 8800 GS using another temperature method

--- NVClockX/NVClock/info.cpp
+++ NVClockX/NVClock/info.cpp
@@ -668,6 +668,7 @@ static const struct pci_ids ids[] =
 	
 	// 1200 - 127F
 	{ 0x1200, "GF100 [GeForce GTX 560 Ti]", DESKTOP },
+    { 0x1201, "GF100 [GeForce GTX 560]", DESKTOP },
 	{ 0x1244, "GF100 [GeForce GTX 550 Ti]", DESKTOP },
 	{ 0x1245, "GF100 [GeForce GTS 450]", DESKTOP },
 	{ 0x1251, "GF100 [N12E-GS-A1]", DESKTOP },
@@ -770,6 +771,7 @@ int get_gpu_arch(int device_id)
 			arch = NV4B; /* 7600 */
 			break;
 		case 0x190:
+        case 0x606:
 			arch = NV50; /* 8800 'NV50 / G80' */
 			break;
 		case 0x400: /* 8600 'G84' */
