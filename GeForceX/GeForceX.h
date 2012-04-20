ed2e3b32724a2dcc5e70b1f3a0ea8915ae091fc7
kozlek <mozodojo@gmail.com>
2012-04-20 20:24:52 +0300

    GeForceX: added men detection, frequencies for other archs

--- GeForceX/GeForceX.h
+++ GeForceX/GeForceX.h
@@ -91,6 +91,38 @@ enum NV50ClockSource {
 	nv50_clk_src_dom6
 };
 
+/* VRAM/fb configuration */
+enum NVVRAMType {
+    NV_MEM_TYPE_UNKNOWN = 0,
+    NV_MEM_TYPE_STOLEN  = 1,
+    NV_MEM_TYPE_SGRAM   = 2,
+    NV_MEM_TYPE_SDRAM   = 3,
+    NV_MEM_TYPE_DDR1    = 4,
+    NV_MEM_TYPE_DDR2    = 5,
+    NV_MEM_TYPE_DDR3    = 6,
+    NV_MEM_TYPE_GDDR2   = 7,
+    NV_MEM_TYPE_GDDR3   = 8,
+    NV_MEM_TYPE_GDDR4   = 9,
+    NV_MEM_TYPE_GDDR5   = 10
+};
+
+static const struct NVVRAMTypes {
+	int value;
+	const char *name;
+} NVVRAMTypeMap[] = {
+    { NV_MEM_TYPE_UNKNOWN, "unknown type" },
+	{ NV_MEM_TYPE_STOLEN , "stolen system memory" },
+	{ NV_MEM_TYPE_SGRAM  , "SGRAM" },
+	{ NV_MEM_TYPE_SDRAM  , "SDRAM" },
+	{ NV_MEM_TYPE_DDR1   , "DDR1" },
+	{ NV_MEM_TYPE_DDR2   , "DDR2" },
+	{ NV_MEM_TYPE_DDR3   , "DDR3" },
+	{ NV_MEM_TYPE_GDDR2  , "GDDR2" },
+	{ NV_MEM_TYPE_GDDR3  , "GDDR3" },
+	{ NV_MEM_TYPE_GDDR4  , "GDDR4" },
+	{ NV_MEM_TYPE_GDDR5  , "GDDR5" }
+};
+
 class GeForceX : public FakeSMCPlugin
 {
     OSDeclareDefaultStructors(GeForceX)    
@@ -106,6 +138,23 @@ private:
     
     UInt32          crystal;
     NVSensorConstants sensor_constants;
+    
+    UInt64          vram_size;
+    NVVRAMType      vram_type;
+    
+    void            nv20_get_vram();
+    void            nvc0_get_vram();
+    UInt32          get_vram_multiplier();
+    
+    UInt32          nv40_read_pll_1(UInt32 reg);
+    UInt32          nv40_read_pll_2(UInt32 reg);
+    UInt32          nv40_read_clk(UInt32 src);
+    UInt32          nv40_get_clock(NVClockSource name);
+    
+    UInt32          nva3_read_vco(int clk);
+    UInt32          nva3_read_clk(int clk, bool ignore_en);
+    UInt32          nva3_read_pll(int clk, UInt32 pll);
+    UInt32          nva3_get_clock(NVClockSource name);
     
     UInt32          nv50_read_div();
     UInt32          nv50_read_pll_src(UInt32 base);
@@ -125,6 +174,7 @@ private:
     int             nv40_get_temperature();
     int             nv84_get_temperature();
     
+    NVVRAMType      nouveau_mem_vbios_type();
     int             score_vbios(const bool writeable);
     void            bios_shadow_pramin();
     void            bios_shadow_prom();
