1364843ff94d429672fec3c1c87265f6c2387744
kozlek <mozodojo@gmail.com>
2012-06-16 23:20:26 +0300

    Better prefs window implementation. Still not responding on drag & drop changes properly (need to click inside the control for changes to take effect)

---
+++ Shared/HWMonitorArrayController.h
@@ -0,0 +1,28 @@
+//
+//  HWMonitorArrayController.h
+//  HWSensors
+//
+//  Created by Kozlek on 16.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HWMonitorArrayController : NSArrayController
+{
+    NSDictionary *_firstFavoriteItem;
+    NSDictionary *_firstAvailableItem;
+}
+
+@property (assign) IBOutlet NSTableView *tableView;
+
+-(void)setFirstFavoriteItem:(NSString*)favoriteName firstAvailableItem:(NSString*)availableName;
+
+-(NSDictionary*)addFavoriteItem:(NSString*)name icon:(NSImage*)icon key:(NSString*)key;
+-(NSDictionary*)addAvailableItem:(NSString*)name icon:(NSImage*)icon key:(NSString*)key;
+
+-(NSArray*)getFavoritesItems;
+
+-(void)setupController;
+
+@end
