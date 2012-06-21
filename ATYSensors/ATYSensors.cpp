6d60a6f324164738f0e2ad9dbc874b40127035d3
kozlek <mozodojo@gmail.com>
2012-06-21 06:02:27 +0300

    Renaming GeForceX to NouveauSensors and RadeonX to ATYSensors. Bumping FakeSMC ver. and compatibile ver. to avoid old plugins loading.

---
+++ ATYSensors/ATYSensors.cpp
@@ -0,0 +1,299 @@
+/*
+ *  Radeon.cpp
+ *  HWSensors
+ *
+ *  Created by Sergey on 20.12.10.
+ *  Copyright 2010 Slice. All rights reserved.
+ *
+ */
+
+#include "ATYSensors.h"
+#include "RadeonChipinfo.h"
+
+#include "FakeSMCDefinitions.h"
+
+#define INVID8(offset)		(mmio_base[offset])
+#define INVID16(offset)		OSReadLittleInt16((mmio_base), offset)
+#define INVID(offset)		OSReadLittleInt32((mmio_base), offset)
+#define OUTVID(offset,val)	OSWriteLittleInt32((mmio_base), offset, val)
+
+#define super FakeSMCPlugin
+OSDefineMetaClassAndStructors(RadeonMonitor, FakeSMCPlugin)
+
+bool RadeonMonitor::initCard()
+{
+	rinfo = (RADEONCardInfo*)IOMalloc(sizeof(RADEONCardInfo));
+	VCard->setMemoryEnable(true);
+	/*	
+	 // PCI dump
+	 for (int i=0; i<0xff; i +=16) {
+     IOLog("%02lx: ", (long unsigned int)i);
+     for (int j=0; j<16; j += 4) {
+     IOLog("%08lx ", (long unsigned int)VCard->configRead32(i+j));
+     }
+     IOLog("\n");
+	 }
+	 */
+	for (UInt32 i = 0; (mmio = VCard->mapDeviceMemoryWithIndex(i)); i++)
+	{
+		long unsigned int mmio_base_phys = mmio->getPhysicalAddress();
+		// Make sure we  select MMIO registers
+		if (((mmio->getLength()) <= 0x00020000) && (mmio_base_phys != 0))
+			break;
+	}
+	if (mmio)
+	{
+		mmio_base = (volatile UInt8 *)mmio->getVirtualAddress();
+	} 
+	else
+	{
+		HWSensorsInfoLog("have no mmio");
+		return false;
+	}
+	
+	if(!getRadeonInfo())
+		return false;
+    
+	switch (rinfo->ChipFamily) {
+        case CHIP_FAMILY_R600:
+        case CHIP_FAMILY_RV610:
+        case CHIP_FAMILY_RV620:
+        case CHIP_FAMILY_RV630:
+        case CHIP_FAMILY_RV670:
+            //setup_R6xx();
+            tempFamily = R6xx;
+            break;
+        case CHIP_FAMILY_R700:
+        case CHIP_FAMILY_R710:
+        case CHIP_FAMILY_RV710:  
+        case CHIP_FAMILY_R730:
+        case CHIP_FAMILY_RV740:
+        case CHIP_FAMILY_RV770:
+        case CHIP_FAMILY_RS780:  
+        case CHIP_FAMILY_RV790:
+            //setup_R7xx();
+            tempFamily = R7xx;
+            break;
+        case CHIP_FAMILY_Evergreen:
+            //setup_Evergreen();
+            tempFamily = R8xx;
+            break;
+            
+        default:
+            HWSensorsInfoLog("sorry, but your card %04lx is not supported!\n", (long unsigned int)(rinfo->device_id));
+            return false;
+    }
+	
+	return true;
+}
+
+bool RadeonMonitor::getRadeonInfo()
+{
+	UInt16 devID = chipID & 0xffff;
+	RADEONCardInfo *devices = radeon_device_list;
+	//rinfo = new RADEONCardInfo;
+	while (devices->device_id != NULL) {
+		//IOLog("check %d/n", devices->device_id ); //Debug
+		if ((devices->device_id & 0xffff) == devID ) {
+			//			rinfo->device_id = devID;
+			rinfo->device_id = devices->device_id;
+			rinfo->ChipFamily = devices->ChipFamily;
+			family = devices->ChipFamily;
+			rinfo->igp = devices->igp;
+			rinfo->is_mobility = devices->is_mobility;
+			HWSensorsInfoLog("found ATI Radeon %04lx", (long unsigned int)devID);
+			return true;
+		}
+		devices++;
+	}
+    
+	HWSensorsWarningLog("unknown device id");
+	return false;
+}
+
+/*
+ void ATICard::setup_R6xx()
+ {
+ char key[5];
+ int id = GetNextUnusedKey(KEY_FORMAT_GPU_DIODE_TEMPERATURE, key);
+ if (id == -1) {
+ HWSensorsInfoLog("No new GPU SMC key!\n");
+ return;
+ }
+ card_number = id;
+ tempSensor = new R6xxTemperatureSensor(this, id, key, TYPE_SP78, 2);
+ Caps = GPU_TEMP_MONITORING;	
+ }
+ 
+ void ATICard::setup_R7xx()
+ {
+ char key[5];
+ int id = GetNextUnusedKey(KEY_FORMAT_GPU_DIODE_TEMPERATURE, key);
+ if (id == -1) {
+ HWSensorsInfoLog("No new GPU SMC key!\n");
+ return;
+ }
+ card_number = id;
+ tempSensor = new R7xxTemperatureSensor(this, id, key, TYPE_SP78, 2);
+ Caps = GPU_TEMP_MONITORING;
+ }
+ 
+ void ATICard::setup_Evergreen()
+ {
+ char key[5];
+ int id = GetNextUnusedKey(KEY_FORMAT_GPU_DIODE_TEMPERATURE, key);
+ if (id == -1) {
+ HWSensorsInfoLog("No new GPU SMC key!\n");
+ return;
+ }
+ card_number = id;
+ tempSensor = new EverTemperatureSensor(this, id, key, TYPE_SP78, 2);
+ Caps = GPU_TEMP_MONITORING;
+ }
+ */
+
+UInt32 RadeonMonitor::read32(UInt32 reg)
+{
+	return INVID(reg);
+}
+
+IOReturn RadeonMonitor::R6xxTemperatureSensor(UInt16* data)
+{
+	UInt32 temp, actual_temp = 0;
+	for (int i=0; i<1000; i++) {  //attempts to ready
+		temp = (read32(CG_THERMAL_STATUS) & ASIC_T_MASK) >> ASIC_T_SHIFT;	
+		if ((temp >> 7) & 1)
+			actual_temp = 0;
+		else {
+			actual_temp = temp & 0xff; //(temp >> 1)
+			break;
+		}
+		IOSleep(10);
+	}
+	*data = (UInt16)(actual_temp & 0xfff);
+	//data[1] = 0;
+	return kIOReturnSuccess; 
+	
+}
+
+IOReturn RadeonMonitor::R7xxTemperatureSensor(UInt16* data)
+{
+	UInt32 temp, actual_temp = 0;
+	for (int i=0; i<1000; i++) {  //attempts to ready
+		temp = (read32(CG_MULT_THERMAL_STATUS) & ASIC_TM_MASK) >> ASIC_TM_SHIFT;	
+		if ((temp >> 9) & 1)
+			actual_temp = 0;
+		else {
+			actual_temp = (temp >> 1) & 0xff;
+			break;
+		}
+		IOSleep(10);
+	}
+	
+	*data = (UInt16)(actual_temp & 0xfff);
+	//data[1] = 0;
+	return kIOReturnSuccess;
+}
+
+IOReturn RadeonMonitor::EverTemperatureSensor(UInt16* data)
+{
+	UInt32 temp, actual_temp = 0;
+	for (int i=0; i<1000; i++) {  //attempts to ready
+		temp = (read32(CG_MULT_THERMAL_STATUS) & ASIC_TM_MASK) >> ASIC_TM_SHIFT;	
+		if ((temp >> 10) & 1)
+			actual_temp = 0;
+		else if ((temp >> 9) & 1)
+			actual_temp = 255;
+		else {
+			actual_temp = (temp >> 1) & 0xff;
+			break;
+		}
+		IOSleep(10);
+	}
+	
+	*data = (UInt16)(actual_temp & 0xfff);
+	//data[1] = 0;
+	return kIOReturnSuccess;
+}
+
+UInt16 RadeonMonitor::readTemperature()
+{
+    UInt16 t = 0;
+    
+    switch (tempFamily) {
+        case R6xx:
+            R6xxTemperatureSensor(&t);
+            break;
+        case R7xx:
+            R7xxTemperatureSensor(&t);
+            break;
+        case R8xx:
+            EverTemperatureSensor(&t);
+            break;
+        default:
+            break;
+    }
+    
+    return t;
+}
+
+
+float RadeonMonitor::getSensorValue(FakeSMCSensor *sensor)
+{
+    if (sensor->getGroup() == kFakeSMCTemperatureSensor)
+        return readTemperature();
+    
+    return 0;
+}
+
+IOService* RadeonMonitor::probe(IOService *provider, SInt32 *score)
+{
+    if (super::probe(provider, score) != this) 
+        return 0;
+    
+    VCard = (IOPCIDevice*)provider;
+    
+    if (!VCard) 
+        return 0;
+    
+    if (OSData *data = OSDynamicCast(OSData, provider->getProperty("device-id")))
+        chipID = *(UInt32*)data->getBytesNoCopy();
+    else {
+        HWSensorsWarningLog("device-id property not found");
+        return 0;
+    }
+    
+    return this;
+}
+
+bool RadeonMonitor::start(IOService * provider)
+{
+	if (!super::start(provider)) 
+        return false;
+    
+    if (!initCard()) {
+        HWSensorsWarningLog("can't initialize driver");
+        return false;
+    }
+    
+    //Find card number
+    SInt8 cardIndex = getVacantGPUIndex();
+    
+    if (cardIndex < 0) {
+        HWSensorsWarningLog("failed to obtain vacant GPU index");
+        return false;
+    }
+    
+    char key[5];
+    
+    snprintf(key, 5, KEY_FORMAT_GPU_PROXIMITY_TEMPERATURE, cardIndex);
+	
+    if (!addSensor(key, TYPE_SP78, 2, kFakeSMCTemperatureSensor, 0)) {
+        HWSensorsWarningLog("can't register temperature sensor for key %s", key);
+        return false;
+    }
+    
+    registerService();
+    
+    return true;
+}
---
+++ NouveauSensors/NouveauSensors.h
@@ -0,0 +1,255 @@
+/*
+ *  NouveauSensors.h
+ *  HWSensors
+ *
+ *  Created by kozlek on 19/04/12.
+ *  Copyright 2010 Natan Zalkin <natan.zalkin@me.com>. All rights reserved.
+ *
+ */
+
+/*
+ * Copyright 2007-2008 Nouveau Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <IOKit/IOService.h>
+#include <IOKit/pci/IOPCIDevice.h>
+#include <IOKit/IOTimerEventSource.h>
+
+#include "FakeSMCPlugin.h"
+
+enum NVBiosType {
+    NVBIOS_BIT,
+    NVBIOS_BMP
+};
+
+enum NVClockSource{
+    NVClockCore     = 1,
+    NVClockShader   = 2,
+    NVClockRop      = 3,
+    NVClockCopy     = 4,
+    NVClockDaemon   = 5,
+    NVClockVdec     = 6,
+    NVCLockMemory   = 8
+};
+
+struct NVBios {
+   	uint8_t*    data;
+	uint32_t    length;
+    NVBiosType  type;
+    uint32_t    offset;
+};
+
+struct NVBitEntry {
+	uint8_t     id;
+	uint8_t     version;
+	uint16_t    length;
+	uint16_t    offset;
+	uint8_t*    data;
+};
+
+struct NVSensorConstants {
+	UInt16 offset_constant;
+	SInt16 offset_mult;
+	SInt16 offset_div;
+	SInt16 slope_mult;
+	SInt16 slope_div;
+    SInt16 temp_critical;
+    SInt16 temp_down_clock;
+    SInt16 temp_fan_boost;
+    SInt16 fan_min_duty;
+    SInt16 fan_max_duty;
+    SInt16 fan_pwm_freq;
+};
+
+enum NV50ClockSource {
+	nv50_clk_src_crystal,
+	nv50_clk_src_href,
+	nv50_clk_src_hclk,
+	nv50_clk_src_hclkm3,
+	nv50_clk_src_hclkm3d2,
+	nv50_clk_src_host,
+	nv50_clk_src_nvclk,
+	nv50_clk_src_sclk,
+	nv50_clk_src_mclk,
+	nv50_clk_src_vdec,
+	nv50_clk_src_dom6
+};
+
+/* VRAM/fb configuration */
+enum NVVRAMType {
+    NV_MEM_TYPE_UNKNOWN = 0,
+    NV_MEM_TYPE_STOLEN  = 1,
+    NV_MEM_TYPE_SGRAM   = 2,
+    NV_MEM_TYPE_SDRAM   = 3,
+    NV_MEM_TYPE_DDR1    = 4,
+    NV_MEM_TYPE_DDR2    = 5,
+    NV_MEM_TYPE_DDR3    = 6,
+    NV_MEM_TYPE_GDDR2   = 7,
+    NV_MEM_TYPE_GDDR3   = 8,
+    NV_MEM_TYPE_GDDR4   = 9,
+    NV_MEM_TYPE_GDDR5   = 10
+};
+
+static const struct NVVRAMTypes {
+	int value;
+	const char *name;
+} NVVRAMTypeMap[] = {
+    { NV_MEM_TYPE_UNKNOWN, "unknown" },
+	{ NV_MEM_TYPE_STOLEN , "stolen system" },
+	{ NV_MEM_TYPE_SGRAM  , "SGRAM" },
+	{ NV_MEM_TYPE_SDRAM  , "SDRAM" },
+	{ NV_MEM_TYPE_DDR1   , "DDR" },
+	{ NV_MEM_TYPE_DDR2   , "DDR2" },
+	{ NV_MEM_TYPE_DDR3   , "DDR3" },
+	{ NV_MEM_TYPE_GDDR2  , "GDDR2" },
+	{ NV_MEM_TYPE_GDDR3  , "GDDR3" },
+	{ NV_MEM_TYPE_GDDR4  , "GDDR4" },
+	{ NV_MEM_TYPE_GDDR5  , "GDDR5" }
+};
+
+struct NVGpioFunc {
+	UInt8 func;
+	UInt8 line;
+	UInt8 log[2];
+};
+
+enum NVDcbGpioTag {
+	DCB_GPIO_PANEL_POWER = 0x01,
+	DCB_GPIO_TVDAC0 = 0x0c,
+	DCB_GPIO_TVDAC1 = 0x2d,
+	DCB_GPIO_PWM_FAN = 0x09,
+	DCB_GPIO_FAN_SENSE = 0x3d,
+	DCB_GPIO_UNUSED = 0xff
+};
+
+struct NVVoltageLevel {
+	UInt32 voltage; /* microvolts */
+	UInt8  vid;
+};
+
+struct NVVoltage {
+	bool supported;
+	UInt8 version;
+	UInt8 vid_mask;
+    
+	struct NVVoltageLevel *level;
+    int nr_level;
+};
+
+class NouveauSensors : public FakeSMCPlugin
+{
+    OSDeclareDefaultStructors(NouveauSensors)    
+	
+private:
+    IOPCIDevice*    device;
+    UInt32          chipset;
+    UInt32          card_type;
+    UInt16          device_id;
+    UInt16          vendor_id;
+    
+    IOMemoryMap*    mmio;
+    
+    NVBios          bios;
+    
+    UInt32          crystal;
+    NVSensorConstants sensor_constants;
+    NVVoltage       voltage;
+    
+    UInt64          vram_size;
+    NVVRAMType      vram_type;
+    
+    bool            g92_asus_temperature;
+      
+    UInt32          nv_rd32(UInt32 reg);
+    void            nv_wr32(UInt32 reg, UInt32 val);
+    UInt32          nv_mask(UInt32 reg, UInt32 mask, UInt32 val);
+    UInt8           nv_rd08(UInt32 reg);
+    void            nv_wr08(UInt32 reg, UInt8 val);
+    
+    
+    void            nouveau_volt_init();
+    float           nouveau_voltage_get();
+    
+    void            nouveau_vram_init();
+    NVVRAMType      nouveau_mem_vbios_type();
+    
+    UInt8 *         dcb_table();
+    UInt8 *         dcb_gpio_table();
+    UInt8 *         dcb_gpio_entry(int idx, int ent, UInt8 *version);
+    bool            nouveau_gpio_func_valid(UInt8 tag);
+    bool            nouveau_gpio_find(int idx, UInt8 func, UInt8 line, struct NVGpioFunc *gpio);
+    UInt32          NVReadCRTC(int head, UInt32 reg);
+    int             nv10_gpio_sense(int line);
+    int             nv50_gpio_sense(int line);
+    int             nvd0_gpio_sense(int line);
+    int             nouveau_gpio_sense(int idx, int line);
+    int             nouveau_gpio_get(int idx, UInt8 tag, UInt8 line);
+    int             nouveau_pwmfan_get();
+    int             nouveau_rpmfan_get(UInt32 milliseconds);
+    bool            nv40_pm_pwm_get(int line, UInt32 *divs, UInt32 *duty);
+    bool            nv50_pm_pwm_get(int line, UInt32 *divs, UInt32 *duty);
+    
+    void            nv20_get_vram();
+    void            nvc0_get_vram();
+    
+    UInt32          nv40_read_pll_1(UInt32 reg);
+    UInt32          nv40_read_pll_2(UInt32 reg);
+    UInt32          nv40_read_clk(UInt32 src);
+    UInt32          nv40_get_clock(NVClockSource name);
+    
+    UInt32          nva3_read_vco(int clk);
+    UInt32          nva3_read_clk(int clk, bool ignore_en);
+    UInt32          nva3_read_pll(int clk, UInt32 pll);
+    UInt32          nva3_get_clock(NVClockSource name);
+    
+    UInt32          nv50_read_div();
+    UInt32          nv50_read_pll_src(UInt32 base);
+    UInt32          nv50_read_pll_ref(UInt32 base);
+    UInt32          nv50_read_pll(UInt32 base);
+    UInt32          nv50_read_clk(NV50ClockSource source);
+    UInt32          nv50_get_clock(NVClockSource name);
+    
+    UInt32          nvc0_read_vco(UInt32 dsrc);
+    UInt32          nvc0_read_pll(UInt32 pll);
+    UInt32          nvc0_read_div(UInt32 doff, UInt32 dsrc, UInt32 dctl);
+    UInt32          nvc0_read_mem();
+    UInt32          nvc0_read_clk(UInt32 clk);
+    UInt32          nvc0_get_clock(NVClockSource name);
+    
+    int             nv40_sensor_setup();
+    int             nv40_get_temperature();
+    int             nv84_get_temperature();
+    int             g92_get_temperature();
+    void            nouveau_temp_init();
+
+    int             score_vbios(const bool writeable);
+    void            bios_shadow_pramin();
+    void            bios_shadow_prom();
+    void            bios_shadow();
+    
+protected:
+    virtual float       getSensorValue(FakeSMCSensor *sensor);
+    
+public:
+    virtual bool		start(IOService *provider);
+    virtual void		free(void);
+};
