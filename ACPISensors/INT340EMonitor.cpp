609c0a6b0bc8f3557138187425e36eaac775409a
kozlek <natan.zalkin@me.com>
2012-08-24 16:55:03 +0300

    Renamed SuperIOSensors into MotherboardSensors. Integrated all GPU monitoring plugins into one: GPUSensors. Preparation for support for Asus/HP/Dell notebooks ACPI monitoring device (PTID "INT340E")

---
+++ ACPISensors/INT340EMonitor.cpp
@@ -0,0 +1,9 @@
+//
+//  INT340E.cpp
+//  HWSensors
+//
+//  Created by Kozlek on 24.08.12.
+//
+//
+
+#include "INT340E.h"
---
+++ ACPISensors/INT340EMonitor.h
@@ -0,0 +1,14 @@
+//
+//  INT340E.h
+//  HWSensors
+//
+//  Created by Kozlek on 24.08.12.
+//
+//
+
+#ifndef __HWSensors__INT340E__
+#define __HWSensors__INT340E__
+
+#include <iostream>
+
+#endif /* defined(__HWSensors__INT340E__) */
