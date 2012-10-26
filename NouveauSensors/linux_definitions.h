b1e12cba75388d8e327c99b9dd0e057eee573062
kozlek <natan.zalkin@me.com>
2012-10-26 19:17:08 +0200

    LPCSensors: fan names fixed for P8H67-M PRO. GPUSensors: debug output disabled, minor bug fixes in vbios module (from nouveaue svn)

--- NouveauSensors/linux_definitions.h
+++ NouveauSensors/linux_definitions.h
@@ -151,4 +151,9 @@ static inline void rep_nop(void)
 
 #define I2C_NAME_SIZE	20
 
+#define min_t(type, x, y) ({                    \
+        type __min1 = (x);                      \
+        type __min2 = (y);                      \
+        __min1 < __min2 ? __min1: __min2; })
+
 #endif
