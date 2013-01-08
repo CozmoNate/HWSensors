4785d3894a44c66bd57fdb9b4bb0c9807e59587c
kozlek <natan.zalkin@me.com>
2013-01-08 17:10:27 +0200

    -

--- GPUSensors/RadeonSensors/radeon_bios.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  radeon_bios.cpp
-//  HWSensors
-//
-//  Created by Natan Zalkin on 08.01.13.
-//
-//
-
-#include "radeon_bios.h"
--- GPUSensors/RadeonSensors/radeon_device.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  radeon.cpp
-//  HWSensors
-//
-//  Created by Natan Zalkin on 08.01.13.
-//
-//
-
-#include "radeon.h"
