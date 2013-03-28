db1c8760bfdea4a9b04eba0e4bfd670fbe995a72
kozlek <natan.zalkin@me.com>
2013-03-28 12:33:04 +0200

    Minor popup rendering changes

---
+++ HWMonitor/FadingButton.h
@@ -0,0 +1,13 @@
+//
+//  FadingButton.h
+//  HWMonitor
+//
+//  Created by kozlek on 28.03.13.
+//  Copyright (c) 2013 kozlek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FadingButton : NSButton
+
+@end
---
+++ HWMonitor/FadingButton.m
@@ -0,0 +1,28 @@
+//
+//  FadingButton.m
+//  HWMonitor
+//
+//  Created by kozlek on 28.03.13.
+//  Copyright (c) 2013 kozlek. All rights reserved.
+//
+
+#import "FadingButton.h"
+
+@implementation FadingButton
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
