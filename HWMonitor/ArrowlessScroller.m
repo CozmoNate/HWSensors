282b416b90bcc993df2207216a049323c245d91b
kozlek <natan.zalkin@me.com>
2013-04-20 15:41:33 +0300

    Better iOS style scrollers

--- HWMonitor/ArrowlessScroller.m
+++ HWMonitor/ArrowlessScroller.m
@@ -10,6 +10,27 @@
 
 @implementation ArrowlessScroller
 
+- (void)drawKnob
+{
+    NSRect rect = [self rectForPart:NSScrollerKnob];
+	
+    if (self.bounds.size.width < self.bounds.size.height) {
+        // vertical scrollbar
+        rect = NSOffsetRect(NSInsetRect(rect, self.bounds.size.width * 0.25, 1), 1, 0);
+    }
+    else {
+        // horizontal scrollbar
+        rect = NSOffsetRect(NSInsetRect(rect, 1, self.bounds.size.height * 0.25), 0, 1);
+    }
+    
+    NSBezierPath* thePath = [NSBezierPath bezierPath];
+    
+    [thePath appendBezierPathWithRoundedRect:rect xRadius:4 yRadius:4];
+    
+    [[NSColor colorWithCalibratedWhite:0.5 alpha:0.5] setFill];    
+    [thePath fill];
+}
+
 - (void)drawRect:(NSRect)dirtyRect
 {
     [self drawKnob];
