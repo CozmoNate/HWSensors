78046c0392d3c1c0374c1c4b129c413d1356fca3
kozlek <natan.zalkin@me.com>
2013-05-04 19:55:37 +0300

    Minor UI tweaks

--- HWMonitor/FadingButton.m
+++ HWMonitor/FadingButton.m
@@ -8,9 +8,9 @@
 
 #import "FadingButton.h"
 
-#define NORMAL_OPACITY  1.0
-#define HOVER_OPACITY   0.9
-#define DOWN_OPACITY    0.8
+#define NORMAL_OPACITY  0.8
+#define HOVER_OPACITY   1.0
+#define DOWN_OPACITY    0.7
 
 @implementation FadingButton
 
