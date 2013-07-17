c6419196a5c70a2702ea435b28461840de17597f
kozlek <natan.zalkin@me.com>
2013-07-17 07:55:57 +0300

    Write/update version.h only then new revision is set

--- Scripts/PreBuild.sh
+++ Scripts/PreBuild.sh
@@ -16,6 +16,10 @@ version_file="${PROJECT_DIR}/Shared/version.h"
 project_name=$(/usr/libexec/PlistBuddy -c "Print 'Project Name'" "${PROJECT_DIR}/version.plist")
 uppercased_name=$(echo $project_name | tr [[:lower:]] [[:upper:]])
 project_version=$(/usr/libexec/PlistBuddy -c "Print 'Project Version'" "${PROJECT_DIR}/version.plist")
+revision_file="${PROJECT_DIR}/revision.txt"
+last_revision=$(<$revision_file)
+
+echo Last project revision: ${last_revision}
 
 cd ${PROJECT_DIR}
 
@@ -27,10 +31,15 @@ then
     sc_revision=$(git rev-list --count HEAD)
 fi
 
-echo "" > ${version_file}
-echo "#define ${uppercased_name}_REVISION ${sc_revision}" >> ${version_file}
-echo "" >> ${version_file}
-echo "#define ${uppercased_name}_VERSION ${project_version}.${sc_revision}" >> ${version_file}
-echo "" >> ${version_file}
-echo "#define ${uppercased_name}_VERSION_STRING \"${project_version}.${sc_revision}\"" >> ${version_file}
-echo "" >> ${version_file}
+if [ "$last_revision" != "$sc_revision" ]
+then
+    echo New project revision: ${sc_revision}
+
+    echo "${sc_revision}" > ${revision_file}
+
+    echo "" > ${version_file}
+    echo "#define ${uppercased_name}_REVISION ${sc_revision}" >> ${version_file}
+    echo "" >> ${version_file}
+    echo "#define ${uppercased_name}_VERSION ${project_version}.${sc_revision}" >> ${version_file}
+    echo "#define ${uppercased_name}_VERSION_STRING \"${project_version}.${sc_revision}\"" >> ${version_file}
+fi
