a40838a2f9716113dfa20d504510199a75b0e850
kozlek <natan.zalkin@me.com>
2013-09-21 08:29:32 +0300

    Starting sparkle integration

--- Scripts/SignApp.sh
+++ Scripts/SignApp.sh
@@ -12,5 +12,8 @@ then
     exit 0
 fi
 
+/usr/libexec/PlistBuddy -c "Add :SUFeedURL string https://raw.github.com/kozlek/HWSensors/develop/Appcast/appcast.xml" "./Binaries/HWMonitor.app/Contents/info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion 5.3.839" "./Binaries/HWMonitor.app/Contents/info.plist"
+
 codesign -s "Developer ID Application" -f ./Binaries/HWMonitor.app
 spctl -a -v --type execute ./Binaries/HWMonitor.app
