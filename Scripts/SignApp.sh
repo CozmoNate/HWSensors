84469a496c4ade25238e72a21f3a6e6e0d7d85f7
kozlek <natan.zalkin@me.com>
2013-09-28 12:50:22 +0300

    Sign also finish_installation.app from Sparkle.framework

--- Scripts/SignApp.sh
+++ Scripts/SignApp.sh
@@ -14,9 +14,8 @@ fi
 
 /usr/libexec/PlistBuddy -c "Add :SUFeedURL string http://hwsensors.com/appcast/appcast.xml" "./Binaries/HWMonitor.app/Contents/info.plist"
 
-# This for testing updates
-#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion 839" "./Binaries/HWMonitor.app/Contents/info.plist"
-#/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString 5.3.839" "./Binaries/HWMonitor.app/Contents/info.plist"
+codesign -s "Developer ID Application" -f ./Binaries/HWMonitor.app/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/finish_installation.app
+spctl -a -v --type execute ./Binaries/HWMonitor.app/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/finish_installation.app
 
 codesign -s "Developer ID Application" -f ./Binaries/HWMonitor.app
 spctl -a -v --type execute ./Binaries/HWMonitor.app
