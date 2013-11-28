2783262548b285ed634da0d539917521698b062b
kozlek <natan.zalkin@me.com>
2013-11-28 23:16:18 +0200

    Minor fixes

---
+++ SmcHelper.h
@@ -0,0 +1,13 @@
+//
+//  SmcHelper.h
+//  HWMonitor
+//
+//  Created by Kozlek on 28/11/13.
+//  Copyright (c) 2013 kozlek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SmcHelper : NSObject
+
+@end
---
+++ SmcHelper.m
@@ -0,0 +1,13 @@
+//
+//  SmcHelper.m
+//  HWMonitor
+//
+//  Created by Kozlek on 28/11/13.
+//  Copyright (c) 2013 kozlek. All rights reserved.
+//
+
+#import "SmcHelper.h"
+
+@implementation SmcHelper
+
+@end
