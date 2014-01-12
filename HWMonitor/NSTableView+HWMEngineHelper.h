a6e0522484084221040d0f7bbdccafe9a8f4a89d
kozlek <natan.zalkin@me.com>
2014-01-12 23:51:17 +0200

    Refactoring

---
+++ HWMonitor/NSTableView+HWMEngineHelper.h
@@ -0,0 +1,13 @@
+//
+//  NSTableView+HWMEngineHelper.h
+//  HWMonitor
+//
+//  Created by Kozlek on 12.01.14.
+//  Copyright (c) 2014 kozlek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSTableView (HWMEngineHelper)
+
+@end
---
+++ HWMonitor/NSTableView+HWMEngineHelper.m
@@ -0,0 +1,13 @@
+//
+//  NSTableView+HWMEngineHelper.m
+//  HWMonitor
+//
+//  Created by Kozlek on 12.01.14.
+//  Copyright (c) 2014 kozlek. All rights reserved.
+//
+
+#import "NSTableView+HWMEngineHelper.h"
+
+@implementation NSTableView (HWMEngineHelper)
+
+@end
