51e1b67e8af23b74380f6b45926c8fe10f8eab81
kozlek <natan.zalkin@me.com>
2014-02-22 18:11:42 +0200

    HWMonitor: working fan control for Mac

---
+++ SmcHelper+HWMonitorHelper.h
@@ -0,0 +1,13 @@
+//
+//  SmcHelper+HWMonitorHelper.h
+//  HWMonitor
+//
+//  Created by Kozlek on 22/02/14.
+//  Copyright (c) 2014 kozlek. All rights reserved.
+//
+
+#import "SmcHelper.h"
+
+@interface SmcHelper (HWMonitorHelper)
+
+@end
---
+++ SmcHelper+HWMonitorHelper.m
@@ -0,0 +1,13 @@
+//
+//  SmcHelper+HWMonitorHelper.m
+//  HWMonitor
+//
+//  Created by Kozlek on 22/02/14.
+//  Copyright (c) 2014 kozlek. All rights reserved.
+//
+
+#import "SmcHelper+HWMonitorHelper.h"
+
+@implementation SmcHelper (HWMonitorHelper)
+
+@end
