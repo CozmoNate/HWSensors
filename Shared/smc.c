a431275e7927e755437a0ab7b4269e0669f897fc
Denis Ahrens <denis@h3q.com>
2014-11-04 01:17:33 +0100

    cache the keytype to safe an extra smccall for every request.
    this code is from smcFanControl

--- Shared/smc.c
+++ Shared/smc.c
@@ -25,7 +25,19 @@ cc ./smc.c  -o smcutil -framework IOKit -framework CoreFoundation -Wno-four-char
 #include <IOKit/IOKitLib.h>
 #include <Kernel/string.h>
 
+#include <libkern/OSAtomic.h>
+
 #include "smc.h"
+
+// Cache the keyInfo to lower the energy impact of SMCReadKey()
+#define KEY_INFO_CACHE_SIZE 100
+struct {
+	UInt32 key;
+	SMCKeyData_keyInfo_t keyInfo;
+} g_keyInfoCache[KEY_INFO_CACHE_SIZE];
+
+int g_keyInfoCacheCount = 0;
+OSSpinLock g_keyInfoSpinLock = 0;
 
 UInt32 _strtoul(const char *str, int size, int base)
 {
@@ -111,35 +123,80 @@ kern_return_t SMCCall(io_connect_t conn, int index, SMCKeyData_t *inputStructure
 									 );
 }
 
+// Provides key info, using a cache to dramatically improve the energy impact of smcFanControl
+kern_return_t SMCGetKeyInfo(io_connect_t conn, UInt32 key, SMCKeyData_keyInfo_t* keyInfo)
+{
+	SMCKeyData_t inputStructure;
+	SMCKeyData_t outputStructure;
+	kern_return_t result = kIOReturnSuccess;
+	int i = 0;
+
+	OSSpinLockLock(&g_keyInfoSpinLock);
+
+	for (; i < g_keyInfoCacheCount; ++i)
+	{
+		if (key == g_keyInfoCache[i].key)
+		{
+			*keyInfo = g_keyInfoCache[i].keyInfo;
+			break;
+		}
+	}
+
+	if (i == g_keyInfoCacheCount)
+	{
+		// Not in cache, must look it up.
+		memset(&inputStructure, 0, sizeof(inputStructure));
+		memset(&outputStructure, 0, sizeof(outputStructure));
+
+		inputStructure.key = key;
+		inputStructure.data8 = SMC_CMD_READ_KEYINFO;
+
+		result = SMCCall(conn, KERNEL_INDEX_SMC, &inputStructure, &outputStructure);
+		if (result == kIOReturnSuccess)
+		{
+			*keyInfo = outputStructure.keyInfo;
+			if (g_keyInfoCacheCount < KEY_INFO_CACHE_SIZE)
+			{
+				g_keyInfoCache[g_keyInfoCacheCount].key = key;
+				g_keyInfoCache[g_keyInfoCacheCount].keyInfo = outputStructure.keyInfo;
+				++g_keyInfoCacheCount;
+			}
+		}
+	}
+
+	OSSpinLockUnlock(&g_keyInfoSpinLock);
+
+	return result;
+}
+
 kern_return_t SMCReadKey(io_connect_t conn, const UInt32Char_t key, SMCVal_t *val)
 {
     kern_return_t result;
     SMCKeyData_t  inputStructure;
     SMCKeyData_t  outputStructure;
-    
+
     memset(&inputStructure, 0, sizeof(SMCKeyData_t));
     memset(&outputStructure, 0, sizeof(SMCKeyData_t));
     memset(val, 0, sizeof(SMCVal_t));
-    
+
     inputStructure.key = _strtoul(key, 4, 16);
-    snprintf(val->key, 5, "%s", key);
-    inputStructure.data8 = SMC_CMD_READ_KEYINFO;
-    
-    result = SMCCall(conn, KERNEL_INDEX_SMC, &inputStructure, &outputStructure);
+    strcpy(val->key, key);
+
+    result = SMCGetKeyInfo(conn, inputStructure.key, &outputStructure.keyInfo);
     if (result != kIOReturnSuccess)
         return result;
-    
+
     val->dataSize = outputStructure.keyInfo.dataSize;
     _ultostr(val->dataType, outputStructure.keyInfo.dataType);
     inputStructure.keyInfo.dataSize = val->dataSize;
     inputStructure.data8 = SMC_CMD_READ_BYTES;
-    
+
     result = SMCCall(conn, KERNEL_INDEX_SMC, &inputStructure, &outputStructure);
     if (result != kIOReturnSuccess)
         return result;
-    
+
     memcpy(val->bytes, outputStructure.bytes, sizeof(outputStructure.bytes));
-    
+
     return kIOReturnSuccess;
 }
 
