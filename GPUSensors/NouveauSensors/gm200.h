85b52b582d15d1378320a7f683145e390b26832d
kozlek <natan.zalkin@me.com>
2014-11-08 20:37:57 +0200

    Some minor changes in project

---
+++ GPUSensors/NouveauSensors/gm200.h
@@ -0,0 +1,15 @@
+//
+//  gm200.h
+//  HWSensors
+//
+//  Created by Kozlek on 24/10/14.
+//
+//
+
+#ifndef __HWSensors__gm200__
+#define __HWSensors__gm200__
+
+bool gm200_identify(struct nouveau_device *device);
+void gm200_init(struct nouveau_device *device);
+
+#endif /* defined(__HWSensors__gm200__) */
