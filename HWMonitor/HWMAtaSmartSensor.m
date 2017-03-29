c5d4ad47a7502bd4c67d31119ef5d1e345e9eace
kozlek <natan.zalkin@me.com>
2017-03-29 15:58:58 +0300

    Copyrights

--- HWMonitor/HWMAtaSmartSensor.m
+++ HWMonitor/HWMAtaSmartSensor.m
@@ -26,6 +26,32 @@
  *
  */
 
+// The parts of this code is based on http://smartmontools.sourceforge.net
+
+/*
+ * Home page of code is: http://smartmontools.sourceforge.net
+ *
+ * Copyright (C) 2002-10 Bruce Allen <smartmontools-support@lists.sourceforge.net>
+ * Copyright (C) 2008-10 Christian Franke <smartmontools-support@lists.sourceforge.net>
+ * Copyright (C) 1999-2000 Michael Cornwell <cornwell@acm.org>
+ * Copyright (C) 2000 Andre Hedrick <andre@linux-ide.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * (for example COPYING); if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * This code was originally developed as a Senior Thesis by Michael Cornwell
+ * at the Concurrent Systems Laboratory (now part of the Storage Systems
+ * Research Center), Jack Baskin School of Engineering, University of
+ * California, Santa Cruz. http://ssrc.soe.ucsc.edu/
+ *
+ */
+
 #import "HWMAtaSmartSensor.h"
 
 #import "HWMEngine.h"
